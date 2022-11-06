b462a4f60bc33a667a803b905609be9bb0bfa4a2
bigliolimatteo <bigliolimatteo@gmail.com>
2022-11-06 22:25:58 +0100

    preprocesser to dataframe

--- processors/DataPreprocesser_old.py
+++ processors/DataPreprocesser_old.py
@@ -17,13 +17,17 @@ class OurStemmer():
 
 class DataPreprocesser():
 
+    # TODO add readibility
     def __init__(self):
         nltk.download('stopwords')
         self.italian_punctuation = punctuation + "’"
         self.stopwords = set(stopwords.words('italian') + stopwords.words('english'))
         self.tokenizer = TweetTokenizer()
         # TODO how to read externally?
-        self.stemmer = OurStemmer(open("custom_tokens.txt", "r").read().split('\n'))
+        self.stemmer = OurStemmer(open("words.txt", "r").read().split('\n'))
+    
+    def remove_emptytweets(self, data):
+        return list(filter(lambda a: a != [], data))
 
     def is_stopword(self, word: str):
         return word in self.stopwords
@@ -34,19 +38,19 @@ class DataPreprocesser():
     def tokenize_tweet(self, tweet: str):
         return [word for word in self.tokenizer.tokenize(tweet) if not self.is_punctuation(word) and not self.is_stopword(word)]
 
-    def tokenize_data(self, data):
-        tokenized_data = data.copy()
-        tokenized_data["text"] = data["text"].map(lambda text: self.tokenize_tweet(text))
-        return tokenized_data
+    def tokenize_data(self, data: dict):
+        POLITICIANS = data.keys()
+        return {politician: self.remove_emptytweets([self.tokenize_tweet(tweet) for tweet in data[politician]]) for politician in POLITICIANS}
 
+    # fixme here tweet is a list? not a string?
     def stem_tweet(self, tweet: list):
         return [self.stemmer.stem(word) for word in tweet] 
 
-    def stem_data(self, data):
-        stemmed_data = data.copy()
-        stemmed_data["text"] = data["text"].map(lambda text: self.stem_tweet(text))
-        return stemmed_data[data["text"].astype(str) != "[]"].drop_duplicates(['text'])
+    # fixme where to put POLITICIANS
+    def stem_data(self, data: dict):
+        POLITICIANS = data.keys()
+        return {politician: self.remove_emptytweets([self.stem_tweet(tweet) for tweet in data[politician]]) for politician in POLITICIANS}
 
-    def preprocess_data(self, data, stem = True):
+    def preprocess_data(self, data: dict, stem = True):
         tokenized_data = self.tokenize_data(data)
         return self.stem_data(tokenized_data) if stem else tokenized_data
