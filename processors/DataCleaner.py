ee1dccced3775f762f354b072e716404a3292652
bigliolimatteo <bigliolimatteo@gmail.com>
2022-11-06 23:19:54 +0100

    bah

--- processors/DataCleaner.py
+++ processors/DataCleaner.py
@@ -8,8 +8,11 @@ def clean_data(data, start_date = datetime(2022,7,22), end_date = datetime(2022,
     data["created_at"] = pd.to_datetime(data["created_at"], format="%Y-%m-%dT%H:%M:%S.%fZ")
     date_filtered_data = data.loc[(data['created_at'] >= start_date) & (data['created_at'] < end_date)]
     
+    # Remove duplicates
+    unique_date_filtered_data = date_filtered_data.drop_duplicates("text")
+
     # Remove retweets
-    non_retweet_date_filtered_data = date_filtered_data[date_filtered_data.apply(lambda row: not row['text'].startswith("RT @"), axis=1)]
+    non_retweet_date_filtered_data = unique_date_filtered_data[unique_date_filtered_data.apply(lambda row: not row['text'].startswith("RT @"), axis=1)]
 
     # Remove links
     non_retweet_date_filtered_data["text"] = non_retweet_date_filtered_data["text"].map(lambda text: re.sub(r'http\S+', '', text))
